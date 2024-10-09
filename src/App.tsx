48a6d832c685ccdc1c414ca77359df91e1f198c8
Kevin Lundin <kevinlundin@comcast.net>
2024-10-08 21:11:23 -0400

    added my name to website

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2> Kevin Lundin</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
