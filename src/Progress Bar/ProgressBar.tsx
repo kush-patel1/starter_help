7f08a8aa4cb03e3b1dff2311ed3a2b8aff947536
kushpatel <kushptl@udel.edu>
2024-10-15 01:26:16 -0400

    implemented the progress bar into the detailed questions page

--- src/Progress Bar/ProgressBar.tsx
+++ src/Progress Bar/ProgressBar.tsx
@@ -5,6 +5,6 @@ type Props={
 export function ProgressBar({ progress }:Props){
     return<div style = {{flexDirection: 'column', display:'flex',alignItems:'center',justifyContent:'center',height:'100%'}}>
         <div style = {{width: '300px', backgroundColor: "gray", height: '10px',borderRadius: '6px',overflow: 'hidden'}}>
-            <div style = {{width: progress.toString().concat('%'), backgroundColor: "blue", height: '100%', transition: 'all'}} />
+            <div style = {{width: progress.toString().concat('%'), backgroundImage: 'linear-gradient(to right, #0a9bae, #12529f)', height: '100%', transition:'width 0.5s ease-in-out'}} />
         </div></div>;
 }
