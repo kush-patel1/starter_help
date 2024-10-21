f340b2adad052214ba090500d066013e0ab3406b
Kevin Lundin <kevinlundin@comcast.net>
2024-10-21 14:13:06 -0400

    Altered basic questions formatting

--- src/Question Pages/basicQuestions.tsx
+++ src/Question Pages/basicQuestions.tsx
@@ -29,6 +29,7 @@ function BasicQuestions(){
         <header className="BasicQuestions-Header">
             <h1>Basic Questions Page</h1>
             <ProgressBar progress={progress} />
+            <p className="checkmark">{progress === 100 ? "✔️": ""}</p>
         </header>
         <body className='BasicQuestions-Body'>
             <ol>
@@ -57,23 +58,62 @@ function BasicQuestions(){
                     <p>Do you enjoy helping others?</p>
                     <Form>
                         <label>
-                            <textarea
-                            className='BasicQuestions-textArea'
+                        <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
                             name="answer2"
-                            value={answers[1]}
+                            value="Yes"
                             onChange={(event) => handleAnswerChange(1, event.target.value)}/>
+                             Yes
+                            <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
+                            name="answer2"
+                            value="No"
+                            onChange={(event) => handleAnswerChange(1, event.target.value)}/>
+                             No
                         </label>
                     </Form>
                 </li>
                 <li>
-                    <p>How important is a high salary to you?</p>
+                    <p>On a scale of 1 to 5, how important is a high salary to you?</p>
                     <Form>
                         <label>
-                            <textarea
-                            className='BasicQuestions-textArea'
+                        <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
                             name="answer3"
-                            value={answers[2]}
+                            value="1"
                             onChange={(event) => handleAnswerChange(2, event.target.value)}/>
+                             1
+                             <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
+                            name="answer3"
+                            value="2"
+                            onChange={(event) => handleAnswerChange(2, event.target.value)}/>
+                             2
+                             <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
+                            name="answer3"
+                            value="3"
+                            onChange={(event) => handleAnswerChange(2, event.target.value)}/>
+                             3
+                             <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
+                            name="answer3"
+                            value="4"
+                            onChange={(event) => handleAnswerChange(2, event.target.value)}/>
+                             4
+                             <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
+                            name="answer3"
+                            value="5"
+                            onChange={(event) => handleAnswerChange(2, event.target.value)}/>
+                             5
                         </label>
                     </Form>
                 </li>
@@ -81,11 +121,20 @@ function BasicQuestions(){
                     <p>Would you rather have a job that is routine or varied?</p>
                     <Form>
                         <label>
-                            <textarea
-                            className='BasicQuestions-textArea'
+                        <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
                             name="answer4"
-                            value={answers[3]}
+                            value="Routine"
                             onChange={(event) => handleAnswerChange(3, event.target.value)}/>
+                             Routine
+                            <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
+                            name="answer4"
+                            value="Varied"
+                            onChange={(event) => handleAnswerChange(3, event.target.value)}/>
+                             Varied
                         </label>
                     </Form>
                 </li>
@@ -93,11 +142,20 @@ function BasicQuestions(){
                     <p>Do you prefer working with your hands or technology?</p>
                     <Form>
                         <label>
-                            <textarea
-                            className='BasicQuestions-textArea'
+                        <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
                             name="answer5"
-                            value={answers[4]}
+                            value="Hands"
                             onChange={(event) => handleAnswerChange(4, event.target.value)}/>
+                             Hands
+                            <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
+                            name="answer5"
+                            value="Technology"
+                            onChange={(event) => handleAnswerChange(4, event.target.value)}/>
+                             Technology
                         </label>
                     </Form>
                 </li>
@@ -105,11 +163,20 @@ function BasicQuestions(){
                     <p>Do you like to solve problems or follow instructions</p>
                     <Form>
                         <label>
-                            <textarea
-                            className='BasicQuestions-textArea'
+                        <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
                             name="answer6"
-                            value={answers[5]}
+                            value="Solving Problems"
                             onChange={(event) => handleAnswerChange(5, event.target.value)}/>
+                             Solving Problems
+                            <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio"
+                            name="answer6"
+                            value="Following Instructions"
+                            onChange={(event) => handleAnswerChange(5, event.target.value)}/>
+                             Following Instructions
                         </label>
                     </Form>
                 </li>
@@ -117,16 +184,25 @@ function BasicQuestions(){
                     <p>Would you rather work in an office or remotely?</p>
                     <Form>
                         <label>
-                            <textarea
-                            className='BasicQuestions-textArea'
+                        <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
                             name="answer7"
-                            value={answers[6]}
+                            value="Office"
                             onChange={(event) => handleAnswerChange(6, event.target.value)}/>
+                             Office
+                            <input 
+                            className='BasicQuestions-MultipleChoice'
+                            type="radio" 
+                            name="answer7"
+                            value="Remote"
+                            onChange={(event) => handleAnswerChange(6, event.target.value)}/>
+                             Remote
                         </label>
                     </Form>
                 </li>
             </ol>
-            <Button className="BasicQuestions-getAnswersButton" onClick={homeClick}> Get Answers </Button>
+            <Button className="BasicQuestions-getAnswersButton" onClick={homeClick} disabled={progress !== 100}> Get Answers </Button>
         </body>
         <footer className='BasicQuestions-Footer'>
           <Button className="Home-Button" onClick={homeClick}> HOME </Button>
